97e4fec7d71e00be61b9fd320da99176373abedd
Yogesh Kumar <yogeshsingh2672000@gmail.com>
2022-05-30 23:08:00 +0530

    used some features of OS module

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
-const path = require("path");
+const path = require("os");
 
-var pathObj = path.parse(__filename);
-console.log(pathObj);
+var totalMemory = path.totalmem();
+var freeMemory = path.freemem();
+// console.log(totalMemory);
+// console.log(freeMemory);
+
+// ES6 feature
+console.log(`Total Memory ${totalMemory}`);
+console.log(`Free Memory ${freeMemory}`);
