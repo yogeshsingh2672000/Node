e5d54bb8048e6a1a6e94fdaf08f347dd364fab8f
Yogesh Kumar <yogeshsingh2672000@gmail.com>
2022-05-30 22:28:56 +0530

    problem with declaring function in VAR

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
-var message = "sadf";
+var say_hello = function () {};
 
-global.console.log(message);
+window.say_hello();
+// avoid declaring this type of function because this is global now with var
+// the problem is when we call this function in another module/file then that function with same name will overried this function and we dont want that
