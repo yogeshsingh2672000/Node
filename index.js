08564d9976e46524789ea475527b462adbead7cb
Yogesh Kumar <yogeshsingh2672000@gmail.com>
2022-08-10 14:39:36 +0530

    learned nodejs connection to mongodb

--- index.js
+++ index.js
@@ -1,24 +1,9 @@
-const express = require("express");
-const app = express();
+const dbConnect = require("./mongo");
 
-const reqFilter = (req, res, next) => {
-  if (!req.query.age) {
-    res.send("please provide age");
-  } else if (req.query.age < 18) {
-    res.send("you are too young for this website");
-  } else if (req.query.age >= 18) {
-    // res.send("<h1>Welcome</h1>");
-    next();
-  }
+const main = async () => {
+  let data = await dbConnect();
+  data = await data.find().toArray();
+  console.log(data);
 };
 
-app.use(reqFilter);
-app.get("", (req, res) => {
-  res.send("Welcome to Home Page");
-});
-
-app.get("/users", (req, res) => {
-  res.send("Welcome to Users Page");
-});
-
-app.listen(4500);
+main();
